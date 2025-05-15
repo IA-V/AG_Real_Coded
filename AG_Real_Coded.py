220602e5af1dbd1cf4d51f759bc3707357656178
IA-V <iagoviper48@gmail.com>
2025-05-15 18:11:07 -0300

    Finalização da seleção por roleta

--- AG_Real_Coded.py
+++ AG_Real_Coded.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 taxa_cruzamento = .7
 taxa_mutacao = .05
@@ -48,21 +49,45 @@ class AG_real_coded:
         pass
 
     def roleta(self): # Seleção de pais para cruzamento
-        qtd_individuos = self.tam_pop * self.taxa_cruzamento
+        qtd_individuos = self.tam_pop * self.taxa_cruzamento # Qtd de novos indivíduos
+        qtd_pais = math.floor(qtd_individuos / 2)  # Qtd de pais para os indivíduos da nova geração (apenas metade da qtd de novos indivíduos, já que o cruzamento gera dois novos indivíduos)
+        pais = [] # Lista de chaves dos indivíduos selecionados para cruzamento
 
         somatorio_pontuacoes = 0 # Total de pontos de todos os indivíduos para definir os que mais contribuem (esses são os melhores - cruzam)
 
         for key, value in self.populacao.items():
             somatorio_pontuacoes += value
 
+        self.populacao = dict(sorted(self.populacao.items(), key=lambda item: item[1])) # Ordem crescente de pontuações
+
+        idx_individuo = self.tam_pop - 1
         
+        while(len(pais) <= qtd_pais): # Itera sobre os indivíduos até adicionar a qtd necessária de pais
+            percentagem = random.uniform(0, 1)
+
+            pontuacao_individuo_atual = list(self.populacao.values())[idx_individuo]
+            chave_individuo_atual = list(self.populacao)[idx_individuo]
+
+            aptidao_individuo_atual = pontuacao_individuo_atual / somatorio_pontuacoes
+
+            if((1-percentagem) <= aptidao_individuo_atual and chave_individuo_atual not in pais): # Condição para selecionar indivíduo atual na roleta
+                pais.append( list(self.populacao)[idx_individuo] )
+
+            if(idx_individuo == -1):
+                idx_individuo = self.tam_pop - 1
+
+            idx_individuo -= 1
+
+        print("Os pais são:\n{}\n".format(pais))
+
+        return pais
 
     def avaliar_cromossomo(self, cromossomo):
         return self.func_avaliacao(cromossomo)
 
     def mostrar_pop(self):
-        for elem in self.populacao: # falta mostrar o valor (tá mostrando somente as chaves, que são as tuplas)
-            print(elem)
+        for key, value in self.populacao.items(): # falta mostrar o valor (tá mostrando somente as chaves, que são as tuplas)
+            print("\n{}:{}\n".format(key, value))
 
 def avaliacao(cromossomo):
     aptidao = 0
@@ -70,11 +95,26 @@ def avaliacao(cromossomo):
         aptidao += elem
 
     return aptidao
+
+# Instanciamento de um objeto AG_real_coded
 ag01 = AG_real_coded(taxa_cruzamento, taxa_mutacao, 10, 5, 0, 6, avaliacao)
 
 ag01.gerar_populacao_inicial()
-ag01.mostrar_pop()
+#ag01.mostrar_pop()
 
 lista_chaves = list(ag01.populacao.keys())
 
 ag01.cruzar(lista_chaves[0], lista_chaves[1], 2) # As chaves já são os genes do cromossomo
+
+pais = ag01.roleta()
+
+if (len(pais) % 2 == 0):
+    for i in range(0, len(pais), 2):
+        if (len(pais) % 2 == 0):
+            ag01.cruzar(lista_chaves[i], lista_chaves[i+1])
+        elif (i == (len(pais)-2) and len(pais) % 2 != 0): # CORRIGIR!!!!
+            ag01.cruzar(lista_chaves[i], lista_chaves[i-1])
+
+ag01.cruzar(lista_chaves[i], lista_chaves[i-1])
+    
+ag01.mostrar_pop()
